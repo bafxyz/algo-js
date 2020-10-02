42e48e3271a39f37251e972a733b1829a12c805e
Andrei Bunulu <bunulu@gmail.com>
2020-10-02 18:27:15 +0200

    feat: steps example added

---
+++ src/udemy/The Coding Interview Bootcamp: Algorithms + Data Structures/steps/index.js
@@ -0,0 +1,48 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+function steps(n) {
+    let count = 0
+
+    while (count !== n) {
+        count++
+
+        console.log(
+            Array.from({ length: n }, (v, k) => (k < count ? '#' : ' ')).join(
+                ''
+            )
+        )
+    }
+}
+
+// function steps(n) {
+//     for (let i = 0; i < n; i++) {
+//         let row = ''
+//         for (let j = 0; j < n; j++) {
+//             if (j <= i) {
+//                 row += '#'
+//             } else {
+//                 row += ' '
+//             }
+//         }
+//         console.log(row)
+//     }
+// }
+
+module.exports = steps
